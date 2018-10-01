90cdf4e25a81d9b2e77d4982a18d0bf36a7bf3ec
thienthongthai <mrthien30@gmail.com>
2018-10-01 14:51:02 +0700

    [Add] - BasicCloudDevice Struct

--- messages/cloud/index.go
+++ messages/cloud/index.go
@@ -4,12 +4,32 @@ type CloudDevice interface {
 	GetDeviceToken() string
 }
 
+type BasicCloudDevice struct {
+	DeviceToken string `json:"device_token"`
+}
+
+func (b BasicCloudDevice) GetDeviceToken() string {
+	return b.DeviceToken
+}
+
 type CloudDevices []CloudDevice
 
 func (c CloudDevices) GetDeviceTokens() []string {
 	result := make([]string, len(c))
 	for i, d := range c {
 		result[i] = d.GetDeviceToken()
+	}
+	return result
+}
+
+func NewBasicCloudDevice(dv string) BasicCloudDevice {
+	return BasicCloudDevice{dv}
+}
+
+func NewBasicCloudDevices(dv []string) []BasicCloudDevice {
+	result := make([]BasicCloudDevice, len(dv))
+	for _, d := range dv {
+		result = append(result, NewBasicCloudDevice(d))
 	}
 	return result
 }
